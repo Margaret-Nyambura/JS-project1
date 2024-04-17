2b489deb3036932d697ad44c5eb20efbf45c115a
Margaret-Nyambura <margaretmacharianyambura@gmail.com>
2024-04-17 15:52:01 +0300

    worked on booleans

---
+++ day-03-coding/booleans.js
@@ -0,0 +1,21 @@
+// Booleans is a data type that outputs two values only
+
+var a = 20;
+var b = 30;
+console.log(a == b);
+
+let sentence = "I am a student"
+console.log(sentence == sentence.toLowerCase());
+console.log(sentence.toUpperCase == sentence);
+
+let number = 500;
+let newNumber = 600;
+console.log(number < newNumber);
+
+let age = 14
+if (age > 18){
+console.log("You are an adult!");
+}
+else{
+    console.log("You are a child!");
+}
