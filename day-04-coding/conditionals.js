cc596d251065800aa7910211825da6519c58c430
Margaret-Nyambura <margaretmacharianyambura@gmail.com>
2024-04-18 15:23:33 +0300

    worked on JavaScript conditions

---
+++ day-04-coding/conditionals.js
@@ -0,0 +1,47 @@
+let marks = 10;
+
+if(marks < 50){
+    console.log("Exam failed!");
+}
+
+else if(marks == 50){
+console.log("Average score!");
+}
+
+else{
+    console.log("Exam passed!");
+}
+
+let age = 1;
+
+if(age >= 0 && age <7){
+    console.log("Student in lower primary school!");
+}
+else if(age >= 8 && age <13){
+    console.log("Student in upper primary school!");
+}
+
+else if(age >= 14 && age <16){
+    console.log("Student in lower secondary school!");
+}
+else if(age >= 17 && age <18){
+    console.log("Student in upper secondary school!");
+}
+else if(age >= 18 && age <22){
+    console.log("Student in upper secondary school!");
+}
+else{
+    console.log("Not a student!");
+}
+
+
+
+
+// IMPORTANT KEY POINTS
+
+// Javascript conditional statements determines whether a code will run or not
+// They controls the behaviours of codes in JavaScript
+// They include IF, ELSE IF, and ELSE
+// IF condition specifies the execution for a block of code. It only runs if the given condition os true
+// ELSE IF specifies a new test if the first condition is false
+// ELSE condition specifies the execution of a code if the condition given is false
