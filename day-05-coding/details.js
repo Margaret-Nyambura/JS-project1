0e983e57742b2e51488e58bb2662edd6201f70d1
Margaret-Nyambura <margaretmacharianyambura@gmail.com>
2024-04-19 15:49:51 +0300

    added files

---
+++ day-05-coding/details.js
@@ -0,0 +1,16 @@
+let heading = "My Coding Journey";
+console.log(heading);
+
+let subContent = "I love JavaScript";
+console.log( alert(subContent));
+
+let content = [
+    {firstName: "Margaret"},
+    {lastName: "Nyambura"},
+    {school: "AkiraChix"},
+    {class: "AnitaB"},
+    {favSubject: "JavaScript"},
+    {town: "Karen"}
+
+]
+console.log(content);
